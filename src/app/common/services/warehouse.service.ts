4eaadc0b3ed17dd46ce0b69d961463ec960b6af1
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-01-19 19:42:26 +0800

    调整库位图界面

--- src/app/common/services/warehouse.service.ts
+++ src/app/common/services/warehouse.service.ts
@@ -25,25 +25,12 @@ export class WarehouseService {
             collapsedIcon: "fa-folder",
             expanded:true,
             children: [
-              { "label": "秦淮区", "icon": "fa-file-word-o", "data": "秦淮区" },
-              { "label": "江宁区", "icon": "fa-file-word-o", "data": "江宁区" },
-              { "label": "建邺区", "icon": "fa-file-word-o", "data": "江宁区" },
-              { "label": "鼓楼区", "icon": "fa-file-word-o", "data": "江宁区" },
-              { "label": "下关区", "icon": "fa-file-word-o", "data": "江宁区" },
-              { "label": "六合区", "icon": "fa-file-word-o", "data": "江宁区" }
-            ]
-          },
-          {
-            label: "南通市",
-            data: "南通市",
-            expandedIcon: "fa-folder-open",
-            collapsedIcon: "fa-folder",
-            expanded:true,
-            children: [
-              { "label": "如皋市", "icon": "fa-file-word-o", "data": "如皋市" },
-              { "label": "海安市", "icon": "fa-file-word-o", "data": "海安市" },
-              { "label": "启东市", "icon": "fa-file-word-o", "data": "启东市" },
-              { "label": "如东市", "icon": "fa-file-word-o", "data": "如东市" }
+              { "label": "京东南京一号库", "icon": "fa-file-word-o", "data": "京东南京一号库" },
+              { "label": "京东南京二号库", "icon": "fa-file-word-o", "data": "京东南京二号库" },
+              { "label": "京东南京三号库", "icon": "fa-file-word-o", "data": "京东南京三号库" },
+              { "label": "京东南京四号库", "icon": "fa-file-word-o", "data": "京东南京四号库" },
+              { "label": "京东南京五号库", "icon": "fa-file-word-o", "data": "京东南京五号库" },
+              { "label": "京东南京六号库", "icon": "fa-file-word-o", "data": "京东南京六号库" }
             ]
           }
         ]
