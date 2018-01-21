e355f6e6157cd8fb620de9213a7df17ec1a0e16f
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-01-21 21:52:27 +0800

    删掉不用的组件

--- src/app/bz-modules/inventory/inventory.module.ts
+++ src/app/bz-modules/inventory/inventory.module.ts
@@ -10,23 +10,14 @@ import { ConfirmDialogModule, ConfirmationService } from 'primeng/primeng';
 import { SharedModule } from '../../common/shared.module';
 import { InventoryComponent } from './inventory.component';
 import { InventoryTableComponent } from './inventory-table/inventory-table.component';
-import { InboundFormComponent } from './inbound-form/inbound-form.component';
-import { ShiftWarehouseTableComponent } from './shift-warehouse-table/shift-warehouse-table.component';
-import { ShiftFormComponent } from './shift-form/shift-form.component';
-import { LossTableComponent } from './loss-table/loss-table.component';
-import { LossFormComponent } from './loss-form/loss-form.component';
 import { InventoryItemFormComponent } from './inventory-item-form/inventory-item-form.component';
 import { InventoryService } from '../../common/services/inventory.service';
-import { InboundService } from '../../common/services/inbound.service';
-import { OutboundService } from '../../common/services/outbound.service';
-import { ShiftWarehouseService } from '../../common/services/shift-warehouse.service';
-import { WarehouseLossService } from '../../common/services/warehouse-loss.service';
-
-import { inventoryRoutes } from './inventory.routes';
 import { InboundReceiptTableComponent } from './inbound-receipt-table/inbound-receipt-table.component';
 import { InboundReceiptDetailComponent } from './inbound-receipt-detail/inbound-receipt-detail.component';
 import { OutboundReceiptTableComponent } from './outbound-receipt-table/outbound-receipt-table.component';
 import { OutboundReceiptDetailComponent } from './outbound-receipt-detail/outbound-receipt-detail.component';
+
+import { inventoryRoutes } from './inventory.routes';
 
 @NgModule({
   imports: [
@@ -41,11 +32,6 @@ import { OutboundReceiptDetailComponent } from './outbound-receipt-detail/outbou
   declarations: [
     InventoryComponent,
     InventoryTableComponent,
-    InboundFormComponent,
-    ShiftWarehouseTableComponent,
-    ShiftFormComponent,
-    LossTableComponent,
-    LossFormComponent,
     InventoryItemFormComponent,
     InboundReceiptTableComponent,
     InboundReceiptDetailComponent,
@@ -54,11 +40,7 @@ import { OutboundReceiptDetailComponent } from './outbound-receipt-detail/outbou
   ],
   providers:[
     ConfirmationService,
-    InventoryService,
-    InboundService,
-    OutboundService,
-    ShiftWarehouseService,
-    WarehouseLossService
+    InventoryService
   ]
 })
 export class InventoryModule { 
--- src/app/bz-modules/inventory/inventory.routes.ts
+++ src/app/bz-modules/inventory/inventory.routes.ts
@@ -1,10 +1,5 @@
 import { InventoryComponent } from './inventory.component';
 import { InventoryTableComponent } from './inventory-table/inventory-table.component';
-import { InboundFormComponent } from './inbound-form/inbound-form.component';
-import { ShiftFormComponent } from './shift-form/shift-form.component';
-import { ShiftWarehouseTableComponent } from './shift-warehouse-table/shift-warehouse-table.component';
-import { LossTableComponent } from './loss-table/loss-table.component';
-import { LossFormComponent } from './loss-form/loss-form.component';
 import { InventoryItemFormComponent } from './inventory-item-form/inventory-item-form.component';
 import { InboundReceiptTableComponent } from './inbound-receipt-table/inbound-receipt-table.component';
 import { InboundReceiptDetailComponent } from './inbound-receipt-detail/inbound-receipt-detail.component';
@@ -18,14 +13,9 @@ export const inventoryRoutes = [{
 		{ path: '', redirectTo: 'inventorytable/page/1', pathMatch: 'full' },
 		{ path: 'inventory-table/page/:page', component: InventoryTableComponent },
 		{ path: 'inventory-item-form', component: InventoryItemFormComponent },
-		{ path: 'inbound-form', component: InboundFormComponent },
 		{ path: 'inbound-receipt-table/page/:page', component: InboundReceiptTableComponent },
 		{ path: 'inbound-receipt-detail/receipt-no/:receipt-no', component: InboundReceiptDetailComponent },
 		{ path: 'outbound-receipt-table/page/:page', component: OutboundReceiptTableComponent },
-		{ path: 'outbound-receipt-detail/receipt-no/:receipt-no', component: OutboundReceiptDetailComponent },
-		{ path: 'shift-form', component: ShiftFormComponent },
-		{ path: 'shift-table/page/:page', component: ShiftWarehouseTableComponent },
-		{ path: 'loss-form', component: LossFormComponent },
-		{ path: 'loss-table/page/:page', component: LossTableComponent }
+		{ path: 'outbound-receipt-detail/receipt-no/:receipt-no', component: OutboundReceiptDetailComponent }
 	]
 }];
