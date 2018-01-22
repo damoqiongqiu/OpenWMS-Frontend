046ad989c9554f4c449a42b3e85f1b8ac50c6dca
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-01-22 13:08:47 +0800

    优化库存功能细节

--- src/app/common/services/inventory.service.ts
+++ src/app/common/services/inventory.service.ts
@@ -10,16 +10,16 @@ export class InventoryService {
 
   public getInventories(warehouseId: string) {
     var items = [
-      { index: 1, warehouseName: '京东南京一号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 2, warehouseName: '京东南京二号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 3, warehouseName: '京东南京三号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 4, warehouseName: '京东南京四号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 5, warehouseName: '京东南京五号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 6, warehouseName: '京东上海一号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 7, warehouseName: '京东上海二号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 8, warehouseName: '京东上海三号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 9, warehouseName: '京东上海四号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'},
-      { index: 10, warehouseName: '京东上海五号库', category: '手机',cellNo:'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999'}
+      { index: 1, warehouseName: '京东南京一号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 2, warehouseName: '京东南京二号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 3, warehouseName: '京东南京三号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 4, warehouseName: '京东南京四号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 5, warehouseName: '京东南京五号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 6, warehouseName: '京东上海一号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 7, warehouseName: '京东上海二号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 8, warehouseName: '京东上海三号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 9, warehouseName: '京东上海四号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' },
+      { index: 10, warehouseName: '京东上海五号库', category: '手机', cellNo: 'A1001', serialNum: '1-222222', itemName: 'iPhone X', itemUnit: '个', spec: '商品规格', costPrice: '2000', bookInventory: '1000', realInventory: '999', diffInventory: '-1' }
     ];
     this.inventories.next(items);
   }
