70dde94cc1476f40907935dc13695cdce867477f
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-01-22 20:33:47 +0800

    优化供应商管理功能细节

--- src/app/bz-modules/basic-data/vendor-table/vendor-table.component.ts
+++ src/app/bz-modules/basic-data/vendor-table/vendor-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SelectItem } from 'primeng/primeng';
+import { ConfirmationService } from 'primeng/primeng';
+import { MessageService } from 'primeng/components/common/messageservice';
 import { VendorService } from '../../../common/services/vendor.service';
 
 @Component({
@@ -13,7 +15,9 @@ export class VendorTableComponent implements OnInit {
 
   constructor(private router: Router,
     private activeRoute: ActivatedRoute,
-    private vendorService: VendorService) { }
+    private vendorService: VendorService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
     this.vendorService.vendors.subscribe((vendors) => {
@@ -22,7 +26,21 @@ export class VendorTableComponent implements OnInit {
     this.vendorService.getVendors();
   }
 
-  public newVendorForm() {
+  public newVendorForm(item) {
     this.router.navigateByUrl('/workspace/basic-data/vendor-form');
+  }
+
+  public editVendor(item) {
+    this.newVendorForm(item);
+  }
+
+  public delVendor(item) {
+    this.confirmationService.confirm({
+      message: '确定要删除吗？',
+      accept: () => {
+        console.log(item);
+        this.messageService.add({ severity: 'success', summary: '成功', detail: '删除数据成功' });
+      }
+    });
   }
 }
