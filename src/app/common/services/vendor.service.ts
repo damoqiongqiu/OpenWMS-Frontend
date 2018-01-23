287a8773ad33cef59152118b8e920e5d1350df6c
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-01-23 21:16:22 +0800

    供应商管理表格改成可展开的行

--- src/app/common/services/vendor.service.ts
+++ src/app/common/services/vendor.service.ts
@@ -10,16 +10,16 @@ export class VendorService {
 
   public getVendors() {
     var vendors = [
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' },
-      { id: '-1', vendorCode: '1-0001', vendorName: '一号供应商', shortName: '一号', vendorAddress: '南京市江宁镇', postcode: '200008', telephone: '12345678900', fax: '123456', email: 'damoqiongqiu@126.com', contact: '大漠穷秋', gender: '男', duty: '销售总监', cellphone: '12345678900', remarks: '这是最重要的一个供应商' }
+      { id: '-1', index: 1, vendorName: '安徽南澳地毯有限公司', addr: '安徽合肥', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 2, vendorName: '蚌埠市双环电子集团有限公司', addr: '安徽蚌埠', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 3, vendorName: '镁联科技（芜湖）有限公司', addr: '安徽芜湖', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 4, vendorName: '芜湖维丰塑料电器有限公司', addr: '安徽芜湖', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 5, vendorName: '福建振中电器制造有限公司', addr: '福建福州', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 6, vendorName: '佛山启新模具厂', addr: '广东佛山', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 7, vendorName: '光普司电子科技有限公司', addr: '广东惠州', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 8, vendorName: '广东大华仁盛科技有限公司', addr: '广东广州', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 9, vendorName: '广州彩虹五金弹簧有限公司', addr: '广东广州', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' },
+      { id: '-1', index: 10, vendorName: '广州市华劲机械有限公司', addr: '广东广州', contact: '李伟建', duty: '销售总监', cellphone: '12345678900', fax: '00000000' }
     ];
     this.vendors.next(vendors);
   }
