e3534d42ffdef63f5d995c81e1b0a752685c8fe3
燕云长风 <hbxyxuxiaodong@163.com>
2019-03-22 20:17:28 +0800

    update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from '../common/services/auth.service';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "../common/services/auth.service";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  public user = { userName: '', password: '' };
-
-  constructor(public router: Router, public activatedRoute: ActivatedRoute, private authService: AuthService) { }
+  public user = { userName: "", password: "" };
+  public fullClass = 'ui-state-filled';
+  @ViewChild('username') username: ElementRef;
+  @ViewChild('pwd') pwd: ElementRef;
+  constructor(
+    public router: Router,
+    public activatedRoute: ActivatedRoute,
+    public render: Renderer2,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.authService.logout();
@@ -18,10 +31,26 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.authService.loggin().subscribe(() => {
-      const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/workspace';
+      const redirect = this.authService.redirectUrl
+        ? this.authService.redirectUrl
+        : "/workspace";
       this.router.navigate([redirect]);
     });
   }
 
-  forgetPwd() { }
+  forgetPwd() {}
+  onUserNameBlur(value) {
+    if (value.trim()) {
+      this.render.addClass(this.username.nativeElement, this.fullClass);
+    } else {
+      this.render.removeClass(this.username.nativeElement, this.fullClass);
+    }
+  }
+  onPasswordBlur(value) {
+    if (value.trim()) {
+      this.render.addClass(this.pwd.nativeElement, this.fullClass);
+    } else {
+      this.render.removeClass(this.pwd.nativeElement, this.fullClass);
+    }
+  }
 }
